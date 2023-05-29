6f06f2577c11f850938332e4b3467e03d353c6ab
Wellington Paiva <wellington192@hotmail.com>
2023-05-29 19:24:17 -0300

    Att login

--- orion_tech/Form1.Designer.vb
+++ orion_tech/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,113 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        PictureBox1 = New PictureBox()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        usuario = New Label()
+        senha = New Label()
+        entrar = New Button()
+        cadastrar = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.IMG_PIM_2_1
+        PictureBox1.Location = New Point(-55, -141)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(530, 526)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(167, 206)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(250, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(167, 245)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(250, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' usuario
+        ' 
+        usuario.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        usuario.AutoSize = True
+        usuario.BackColor = SystemColors.ControlLightLight
+        usuario.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        usuario.ForeColor = SystemColors.MenuHighlight
+        usuario.Location = New Point(73, 199)
+        usuario.Name = "usuario"
+        usuario.Size = New Size(88, 30)
+        usuario.TabIndex = 3
+        usuario.Tag = ""
+        usuario.Text = "Usuário"
+        ' 
+        ' senha
+        ' 
+        senha.AutoSize = True
+        senha.BackColor = SystemColors.ControlLightLight
+        senha.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        senha.ForeColor = SystemColors.MenuHighlight
+        senha.Location = New Point(88, 236)
+        senha.Name = "senha"
+        senha.Size = New Size(73, 30)
+        senha.TabIndex = 4
+        senha.Tag = ""
+        senha.Text = "Senha"
+        ' 
+        ' entrar
+        ' 
+        entrar.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        entrar.ForeColor = SystemColors.HotTrack
+        entrar.Location = New Point(337, 288)
+        entrar.Name = "entrar"
+        entrar.Size = New Size(80, 35)
+        entrar.TabIndex = 5
+        entrar.Text = "Entrar"
+        entrar.UseVisualStyleBackColor = True
+        ' 
+        ' cadastrar
+        ' 
+        cadastrar.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        cadastrar.ForeColor = SystemColors.HotTrack
+        cadastrar.Location = New Point(217, 288)
+        cadastrar.Name = "cadastrar"
+        cadastrar.Size = New Size(94, 35)
+        cadastrar.TabIndex = 6
+        cadastrar.Text = "Cadastrar"
+        cadastrar.UseVisualStyleBackColor = True
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(467, 380)
+        Controls.Add(cadastrar)
+        Controls.Add(entrar)
+        Controls.Add(senha)
+        Controls.Add(usuario)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(PictureBox1)
+        Name = "form1"
+        Text = "Login"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents usuario As Label
+    Friend WithEvents senha As Label
+    Friend WithEvents entrar As Button
+    Friend WithEvents cadastrar As Button
 End Class
--- orion_tech/Form1.vb
+++ orion_tech/Form1.vb
@@ -1,3 +1,29 @@
-﻿Public Class Form1
+﻿Public Class form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.SizeModeChanged, PictureBox1.SizeChanged, PictureBox1.Click
 
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles usuario.Click
+
+    End Sub
+
+    Private Sub Label1_Click_2(sender As Object, e As EventArgs) Handles senha.Click
+
+    End Sub
+
+    Private Sub entrar_Click(sender As Object, e As EventArgs) Handles entrar.Click
+
+    End Sub
+
+    Private Sub cadastrar_Click(sender As Object, e As EventArgs) Handles cadastrar.Click
+
+    End Sub
 End Class
